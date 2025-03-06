57d58b411d97e06365b93537d2572101d8a4243b
Ricardo Mendes <ricardolsmendes@gmail.com>
2025-03-06 13:47:36 -0300

    refactor: replace external with managed tables (#3)
    Co-authored-by: ricardolsmendes <ricardolsmendes>

--- config/Data Extract.py
+++ config/Data Extract.py
@@ -8,13 +8,13 @@
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC Set Kaggle credential configuration values in the block below: You can set up a [secret scope](https://docs.databricks.com/security/secrets/secret-scopes.html) to manage credentials used in notebooks. For the block below, we have manually set up the `fine-grained-df-dev` secret scope and saved our credentials there for internal testing purposes.
+# MAGIC Set Kaggle credential configuration values in the block below: You can set up a [secret scope](https://docs.databricks.com/security/secrets/secret-scopes.html) to manage credentials used in notebooks. For the block below, we have set up the `fgdf-accelerator-dev` secret scope using Terraform and saved our credentials there for internal testing purposes.
 
 # COMMAND ----------
 
 import os
 
-secret_scope = "fine-grained-df-dev"
+secret_scope = "fgdf-accelerator-dev"
 
 # os.environ["KAGGLE_USERNAME"] = "YOUR KAGGLE USERNAME HERE" # replace with your own credential here temporarily or set up a secret scope with your credential
 os.environ["KAGGLE_USERNAME"] = dbutils.secrets.get(secret_scope, "kaggle-username")
@@ -40,13 +40,19 @@ os.environ["KAGGLE_KEY"] = dbutils.secrets.get(secret_scope, "kaggle-key")
 
 # COMMAND ----------
 
-# MAGIC %md Move the downloaded data to the landing folder used throughout the accelerator:
+# MAGIC %md Move the downloaded data to the bronze layer used throughout the accelerator:
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC CREATE VOLUME IF NOT EXISTS `fgdf_accelerator_dev`.`bronze`.`kaggle`;
 
 # COMMAND ----------
 
 downloaded_file_path = "/databricks/driver/train.csv"
 
-catalog = "fine_grained_df_dev"
-landing_file_path = f"/Volumes/{catalog}/landing/blob_storage/kaggle/train.csv"
+catalog = "fgdf_accelerator_dev"
+landing_file_path = f"/Volumes/{catalog}/bronze/kaggle/train.csv"
 
 dbutils.fs.cp(f"file:{downloaded_file_path}", landing_file_path)
